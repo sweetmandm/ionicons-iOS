7f6bf0e5d5a17b4f6ef315af83842daae9aee5dd
Chase Wang <chase@ocwang.com>
2015-04-18 14:08:11 -0400

    Added tests for checking if glyphs exist for icon names.

--- Example-ionicons/Example-ioniconsTests/Example_ioniconsTests.m
+++ Example-ionicons/Example-ioniconsTests/Example_ioniconsTests.m
@@ -9,6 +9,7 @@
 #import <XCTest/XCTest.h>
 #import "allIconCodes.h"
 #import "IonIcons.h"
+@import CoreText;
 
 @interface Example_ioniconsTests : XCTestCase
 
@@ -38,6 +39,37 @@
 {
     UIFont* fontName = [IonIcons fontWithSize:15.0];
     XCTAssertNotNil(fontName);
+}
+
+- (void)testIconNamesReturnGlyphs
+{
+    NSArray *iconNamesArray = [self iconNamesArray];
+    UIFont *font = [IonIcons fontWithSize:10];
+    
+    for (NSString *iconName in iconNamesArray) {
+        for (int i = 0; i < iconName.length; i++) {
+            BOOL doesGlyphExist = [self doesGlyph:[iconName characterAtIndex:i] existInFont:font];
+            XCTAssertNotEqual(doesGlyphExist, NO, @"Glyph doesn't exist");
+        }
+    }
+}
+
+- (BOOL)doesGlyph:(unichar)character existInFont:(UIFont *)font
+{
+    UniChar characters[] = { character };
+    CGGlyph glyphs[1] = { };
+    CTFontRef ctFont = CTFontCreateWithName((CFStringRef)font.fontName, font.pointSize, NULL);
+    
+    BOOL ret = CTFontGetGlyphsForCharacters(ctFont, characters, glyphs, 1);
+    
+//    CFRelease(ctFont);
+    
+    return ret;
+}
+
+- (NSArray *)iconNamesArray
+{
+    return @[ion_android_cart, ion_android_add_circle];
 }
 
 @end
